a64d235ba5d78f0d40c79aedb7f2b8b1211371a1
Adithya Baliga B <58630551+adibaliga@users.noreply.github.com>
2025-06-09 00:07:18 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Sticky Navigation Bar: Create a navigation bar that sticks to the top of the page as the user scrolls down
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex w-full h-[1100px] justify-center">
+      <nav className="w-full">
+        <ul className="flex gap-2 justify-center sticky top-0 bg-amber-800 w-full text-white">
+          <li>Home</li>
+          <li>About</li>
+          <li>Privacy</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
