502a3fdcccb8056113dee7acccce0b81919ecfc6
Adithya Baliga B <58630551+adibaliga@users.noreply.github.com>
2025-06-09 00:20:05 +0530

    Enhance navigation bar with links and styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="flex w-full h-[1100px] justify-center">
-      <nav className="w-full">
-        <ul className="flex gap-2 justify-center sticky top-0 bg-amber-800 w-full text-white">
-          <li>Home</li>
-          <li>About</li>
-          <li>Privacy</li>
+    <div className="flex w-full h-[1100px] justify-center ">
+      <nav className="w-full h-10 bg-amber-800 flex items-center ">
+        <ul className="flex gap-6 justify-center sticky top-0  w-full text-white ">
+          <li className="cursor-pointer">
+            <a href="#home">Home</a>
+          </li>
+          <li className="cursor-pointer">
+            <a href="#about">About</a>
+          </li>
+          <li className="cursor-pointer">
+            <a href="#privacy">Privacy</a>
+          </li>
         </ul>
       </nav>
     </div>
